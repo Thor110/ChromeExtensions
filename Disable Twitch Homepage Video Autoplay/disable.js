2464a2c341f8a5a1404fc1efb5a8064ff1db1c83
Edward James Gordon <invaderzimtwo@hotmail.co.uk>
2025-02-04 03:36:53 +0000

    Twitch Disable Autoplay

--- Disable Twitch Homepage Video Autoplay/disable.js
+++ Disable Twitch Homepage Video Autoplay/disable.js
@@ -1,13 +1,25 @@
-// contentScript.js
+console.log('Content script injected');
+
+let buttonClicked = false;
+
 function disableLoop() {
-  const label = document.querySelector('.ytp-menuitem[aria-label="Play (space/k)"]');
-  if (label) {
-    label.setAttribute('aria-label', 'Pause (space/k)');
-  }
-  const state = document.querySelector('.ytp-menuitem[data-a-player-state="playing"]');
-  if (state) {
-    state.setAttribute('data-a-player-state', 'paused');
+  console.log('disableLoop function called');
+  const button = document.querySelector('[data-a-target="player-play-pause-button"]');
+  console.log('Button query result:', button);
+
+  if (button &&!buttonClicked) {
+    console.log('Button found');
+    buttonClicked = true;
+    setTimeout(() => {
+      button.click();
+      console.log('Button click event triggered');
+    }, 1000);
+  } else if (button) {
+    console.log('Button already clicked');
+    clearInterval(myInterval);
+  } else {
+    console.log('Button not found');
   }
 }
 
-document.addEventListener('DOMContentLoaded', disableLoop);
+const myInterval = setInterval(disableLoop, 1000);
