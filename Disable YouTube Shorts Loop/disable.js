303ca1d5ba9fb53152c7d24faac59f3d97855c81
Edward James Gordon <invaderzimtwo@hotmail.co.uk>
2025-02-04 14:55:25 +0000

    No Event Listener Loop

--- Disable YouTube Shorts Loop/disable.js
+++ Disable YouTube Shorts Loop/disable.js
@@ -1,14 +1,17 @@
 // contentScript.js
+console.log('Content script injected');
 function disableLoop() {
+  console.log('disableLoop function called');
   const loopCheckbox = document.querySelector('.ytp-menuitem[role="menuitemcheckbox"]');
   if (loopCheckbox) {
     loopCheckbox.setAttribute('aria-checked', 'false');
+	console.log('aria-checked set to false');
   }
   const video = document.querySelector('video');
   if (video) {
     video.removeAttribute('loop');
+	console.log('loop attribute removed');
   }
 }
 
-document.addEventListener('DOMContentLoaded', disableLoop);
 setInterval(disableLoop, 100);
